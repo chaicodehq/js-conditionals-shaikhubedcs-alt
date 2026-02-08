147c364133e43d8bd83ee621104013f6bb408a6c
Shaikh Ubed <shaikhubedcs@gmail.com>
2026-02-08 18:30:40 +0530

    completed until 09-password

--- src/09-password-strength.js
+++ src/09-password-strength.js
@@ -26,5 +26,49 @@
  * @returns {string} "weak", "medium", "strong", or "very strong"
  */
 export function checkPasswordStrength(password) {
-  // Your code here
+  if(typeof password !== "string" || password === null || password === undefined || password.trim() === ""|| password.length === 0){
+  return 'weak';
+  }
+  let criteriaMet =0;
+  if(password.length>=8)
+  {
+    criteriaMet++;
+
+  }
+  if(/[A-Z]/.test(password))
+  {
+    criteriaMet++;
+
+  }
+  if(/[a-z]/.test(password))
+  {
+    criteriaMet++;  
+  }
+  if(/[0-9]/.test(password))
+  {
+    criteriaMet++;
+  }
+  if(/[!@#$%^&*()_+\-=\[\]{}|;:,.<>?]/.test(password))
+  {
+    criteriaMet++;
+  }
+  if(criteriaMet <=1)
+    {
+      return 'weak';
+    }
+  if(criteriaMet <=3 )
+  {
+    return 'medium';
+  }
+  if(criteriaMet ===4)
+  {
+    return 'strong';
+  }
+   if(criteriaMet ===5)
+  {
+    return 'very strong';
+  }
+ 
+  return 'weak';
 }
+
